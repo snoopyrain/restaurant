38378507e655c8051f739d1c71f367aa4dbb31af
林玉堂 <>
2019-04-22 21:16:58 -0700

    init express

---
+++ app.js
@@ -0,0 +1,20 @@
+
+//include express , handlebars
+const express = require('express')
+const app = express()
+const exphbs = require('express-handlebars')
+const port = 3000
+
+//setting template engine
+app.engine('handlebars', exphbs({defaultLayout:'main'}))
+app.set('view engine', 'handlebars')
+
+//setting routes
+
+app.get('/',(req, res)=>{
+  res.render('index')
+})
+
+app.listen(port, ()=>{
+  console.log(`Express app listening on port ${port}.`)
+})
