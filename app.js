77ae8685cc1fa6f76b322a6d67597b7f2b4035f0
林玉堂 <>
2019-04-23 22:38:08 -0700

    using json and add searcrch function

--- app.js
+++ app.js
@@ -14,9 +14,25 @@ app.set('view engine', 'handlebars')
 //setting routes for get
 
 app.get('/',(req, res)=>{
-  res.render('index')
+  res.render('index',{restaurants: restaurantList.results})
+})
+
+app.get('/restaurants/:restaurant_id', (req, res) => {
+  const restaurant = restaurantList.results.filter(rest=>rest.id==req.params.restaurant_id)
+  console.log(restaurant)
+  res.render('show', { restaurant: restaurant[0] })
+})
+
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword
+  console.log(keyword)
+  const restaurant =restaurantList.results.filter(
+    res=> {return res.name.toLowerCase().includes(keyword.toLowerCase())})
+  console.log(restaurant)
+    res.render('index',{restaurants: restaurant})
 })
 
 app.listen(port, ()=>{
   console.log(`Express app listening on port ${port}.`)
 })
+
